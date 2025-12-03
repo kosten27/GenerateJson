b603e3a6819d1f6bed6b671d4253bb0f33c12b1e
kosten27 <kosten27@gmail.com>
2025-12-03 17:30:01 +0200

    Renamed files

---
+++ src/main/kotlin/RenameImages.kt
@@ -0,0 +1,29 @@
+import org.example.SectionImages
+import java.io.File
+
+fun main() {
+    val imageNames = File("src/main/resources/image_names.txt").readText()
+    val lines = imageNames.trim().lines()
+    val sectionRegex = Regex("""^(\d+(.\d)?):(.*)$""")
+
+    lines.forEach { line ->
+        val match = sectionRegex.matchEntire(line) ?: return
+
+        val sectionId = match.groupValues[1].replace(".", "_")
+        val raw = match.groupValues[3].trim()
+
+        if (raw.isEmpty()) {
+            return
+        } else {
+            raw.split(",")
+                .forEach { part ->
+                    val (questionNumber, imageNumber) = part.split("-")
+                    val originalImageName = "image_${imageNumber}.jpg"
+                    val newImageName = "image_s${sectionId}_q${questionNumber}.jpg"
+                    val bytes = File("src/main/resources/original_images/${originalImageName}").readBytes()
+                    File("src/main/resources/renamed_images/${newImageName}").writeBytes(bytes)
+                }
+
+        }
+    }
+}
